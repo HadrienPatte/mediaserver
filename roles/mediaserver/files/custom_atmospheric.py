d53f90b0b505b4b0c5328aefe21b5493e5eeab4a
Hadrien Patte <hadrien.patte@protonmail.com>
2023-06-16 01:08:57 +0200

    Optimize custom_atmospheric datadog check

--- roles/mediaserver/files/custom_atmospheric.py
+++ roles/mediaserver/files/custom_atmospheric.py
@@ -6,17 +6,20 @@ from checks import AgentCheck
 class AtmosphericCheck(AgentCheck):
     __NAMESPACE__ = "atmospheric"
 
-    def check(self, instance):
+    def __init__(self, name, init_config, instances):
+        super(AtmosphericCheck, self).__init__(name, init_config, instances)
+
         port = self.instance.get("port", 1)
-        address = self.instance.get("address", 0x76)
-        tags = self.instance.get("tags", [])
+        self.address = self.instance.get("address", 0x76)
+        self.tags = self.instance.get("tags", [])
 
-        bus = smbus2.SMBus(port)
+        self.bus = smbus2.SMBus(port)
 
-        calibration_params = bme280.load_calibration_params(bus, address)
+        self.calibration_params = bme280.load_calibration_params(self.bus, self.address)
 
-        data = bme280.sample(bus, address, calibration_params)
+    def check(self, _):
+        data = bme280.sample(self.bus, self.address, self.calibration_params)
 
-        self.gauge("temperature", data.temperature, tags=tags)
-        self.gauge("pressure", data.pressure, tags=tags)
-        self.gauge("humidity", data.humidity, tags=tags)
+        self.gauge("temperature", data.temperature, tags=self.tags)
+        self.gauge("pressure", data.pressure, tags=self.tags)
+        self.gauge("humidity", data.humidity, tags=self.tags)
